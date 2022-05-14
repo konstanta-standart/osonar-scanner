0249124876636e59c1c4e37db60b11ea3dc8c94c
Антон Степанов <stepa86@gmail.com>
2022-05-14 15:37:49 +0300

    нов: Установка ПО

---
+++ sonar/InstallUpdateOs.bat
@@ -0,0 +1,15 @@
+@chcp 65001>nul
+
+@call opm install cmdline
+@call opm install 1commands
+@call opm install v8metadata-reader
+@call opm install logos
+@call opm install configor
+@call opm install stebi
+
+@call opm update cmdline
+@call opm update 1commands
+@call opm update v8metadata-reader
+@call opm update logos
+@call opm update configor
+@call opm update stebi
