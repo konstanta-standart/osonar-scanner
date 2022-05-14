9f6e9c256925a55c840242b5dbffdc08c2e134d3
Антон Степанов <stepa86@gmail.com>
2022-05-14 17:33:59 +0300

    фикс: Замена читателя параметров

--- sonar/InstallUpdateOs.bat
+++ sonar/InstallUpdateOs.bat
@@ -1,17 +1,13 @@
-@chcp 65001>nul
-
 @set LOGOS_CONFIG=logger.oscript.app.opm=ERROR;
 
 @call opm install cmdline
 @call opm install 1commands
 @call opm install v8metadata-reader
 @call opm install logos
-@call opm install configor
 @call opm install stebi
 
 @call opm update cmdline
 @call opm update 1commands
 @call opm update v8metadata-reader
 @call opm update logos
-@call opm update configor
 @call opm update stebi
