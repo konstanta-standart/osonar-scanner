9e4879b0514e5fac0eb99c641999897d3784e2fa
Антон Степанов <stepa86@gmail.com>
2022-05-14 18:36:01 +0300

    вип: Кодировки

--- sonar/InstallUpdateOs.bat
+++ sonar/InstallUpdateOs.bat
@@ -4,10 +4,12 @@
 @call opm install 1commands
 @call opm install v8metadata-reader
 @call opm install logos
+@call opm install tempfiles
 @call opm install stebi
 
 @call opm update cmdline
 @call opm update 1commands
 @call opm update v8metadata-reader
 @call opm update logos
+@call opm update tempfiles
 @call opm update stebi
